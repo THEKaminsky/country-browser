b8d6e2cb77ae6ab64d3f71cbc7c1c473b129cd71
Kaminsky <rjkaminskyjr@gmail.com>
2025-04-28 17:30:54 -0600

    test: countries api call tests for results or errors

---
+++ src/api/countries.test.js
@@ -0,0 +1,9 @@
+import { expect, describe } from 'vitest'
+import getCountries from './countries';
+
+describe('Countries API', () => {
+  it('should fetch data correctly from the REST Countries API', async () => {
+    const countries = await getCountries();
+    expect(Array.isArray(countries)).toBe(true);
+  });
+})
