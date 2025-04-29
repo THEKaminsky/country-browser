b91afef9951a3f3169b3a47a45ea6336f3af3c1b
Kaminsky <rjkaminskyjr@gmail.com>
2025-04-28 18:39:53 -0600

    refactor: clean up and add types

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import { Country } from '../types/country';
 
 const API_URL = 'https://restcountries.com/v3.1/all';
 
-async function getCountries() {
+async function getCountries(): Promise<Country[]> {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get<Country[]>(API_URL);
     return response.data;
   } catch (error) {
     console.error('Error fetching countries:', error);
-    throw error;
+    return [];
   }
 }
 
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import getCountries from '../api/countries';
+import { Country } from '../types/country';
 
 const CountryList = () => {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
     getCountries().then(data => {
@@ -14,7 +15,7 @@ const CountryList = () => {
 
   return (
     <ul>
-      {countries.map((country) => (
+      {countries.map((country: Country) => (
         <li key={country.name.common}>
           <span>{country.name.common}</span>
         </li>
